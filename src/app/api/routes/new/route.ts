be6279ca3fc86d221e47a5228dc5471b00a530e5
Samuel <khobewayne@gmail.com>
2025-03-21 17:36:50 +0000

    create post route and model complete

--- src/app/api/routes/new/route.ts
+++ src/app/api/routes/new/route.ts
@@ -1,11 +1,10 @@
 import { NextResponse, NextRequest } from "next/server";
-import { createBlogPost } from "../../models/blog.model";
-
+import { createPost } from "../../models/model";
 export async function POST(request: NextRequest) {
     const body = await request.json();
     console.log(body)
     try {
-        const newPost = await createBlogPost(body);
+        const newPost = createPost(body);
         return NextResponse.json({status: 200, body: newPost, message: "Blog post created successfully"});
     } catch(error){
         console.error("Error creating blog post:", error);
